6254b399b26236925740f651e9f9135e1137dc4f
Michaël L <michaellaurac@gmail.com>
2022-06-07 22:08:32 +0200

    Added a lookUp member function in the superclass checking for the instance of the class

--- word.js
+++ word.js
@@ -1,11 +1,19 @@
 class Word {
   constructor (word) {
     this.word = word;
-  }
+  };
 
   count () {
     return this.word.length;
-  }
+  };
+
+  lookUp () {
+    if (this instanceof JapaneseWord) {
+      return `http://jisho.org/search/${this.word}`;
+    } else {
+      return `http://en.wiktionary.org/wiki/${this.word}`;
+    }
+  };
 };
 
 class EnglishWord extends Word {};
@@ -15,5 +23,7 @@ const japaneseWord = new JapaneseWord('犬');
 const englishWord = new EnglishWord('dog');
 console.log(japaneseWord.word);
 console.log(japaneseWord.count());
+console.log(japaneseWord.lookUp());
 console.log(englishWord.word);
 console.log(englishWord.count());
+console.log(englishWord.lookUp());
