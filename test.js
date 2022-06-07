427fba6b6a6e65b7b8b08254f686794070fee9c7
Michaël L <michaellaurac@gmail.com>
2022-06-07 21:41:01 +0200

    Initial setup including the filename test passing and style showing no problems

---
+++ test.js
@@ -0,0 +1,17 @@
+const fileName = () => {
+  const theError = new Error('here I am');
+  return /\\(\w+\.js):/.exec(theError.stack)[1];
+};
+
+const welcomeMessage = () => {
+  return `Welcome to ${fileName()}!`;
+};
+
+/* eslint-env mocha */
+
+const wish = require('wish');
+describe('the file', () => {
+  it('sets welcome message', () => {
+    wish(welcomeMessage() === 'Welcome to test.js!');
+  });
+});
