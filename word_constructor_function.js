524e53a25795154e7808adc35cdc8c193e4b07d0
Michaël L <michaellaurac@gmail.com>
2022-06-08 21:28:23 +0200

    Generated the same structure in a different file using constructor functions

---
+++ word_constructor_function.js
@@ -0,0 +1,24 @@
+function Word (word, language, loopUpUrl) {
+  this.word = word;
+  this.language = language;
+  this.loopUpUrl = loopUpUrl;
+  this.count = function () {
+    return this.word.length;
+  };
+  this.lookUp = function () {
+    return this.loopUpUrl + this.word;
+  };
+};
+
+function EnglishWord (word) {
+  Word.call(this, word, "English", "http://en.wiktionary.ord/wiki/");
+}
+
+function JapaneseWord (word) {
+  Word.call(this, word, "Japanese", "http://jisho.org/search/");
+}
+
+JapaneseWord.prototype = Object.create(Word.prototype);
+JapaneseWord.prototype.constructor = JapaneseWord;
+EnglishWord.prototype = Object.create(Word.prototype);
+EnglishWord.prototype.constructor = EnglishWord;
