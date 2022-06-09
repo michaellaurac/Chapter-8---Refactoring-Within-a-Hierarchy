52a2e1040ddf4e83ee168ea969eb386d4b677673
Michaël L <michaellaurac@gmail.com>
2022-06-09 16:31:30 +0200

    Used object literals to create an english and a japanese word from the same word object

---
+++ word_object_literal.js
@@ -0,0 +1,18 @@
+const word = {
+  count () {
+    return this.word.length;
+  },
+  lookUp () {
+    return this.lookUpUrl + this.word;
+  }
+};
+
+const englishWord = Object.create(word);
+englishWord.word = "dog";
+englishWord.language = "English";
+englishWord.lookUpUrl = "http://en.wiktionary.org/wiki/";
+
+const japaneseWord = Object.create(word);
+japaneseWord.word = "犬";
+japaneseWord.language = "Japanese";
+japaneseWord.lookUpUrl = "http://jisho.org/search/";
